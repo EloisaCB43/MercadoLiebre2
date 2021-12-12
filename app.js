94764bf382cabb13f9bf831a220315bc28e510dc
EloisaCB43 <eloisacardona43@gmail.com>
2021-12-12 11:52:08 -0500

    agregué login.html y register.html, tambien hice el login

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const res = require('express/lib/response');
+const { dirname } = require('path');
 const app = express();
 const path= require('path')
 app.listen(3000,() => console.log ('servidor corriendo en el puerto 3000'));
@@ -7,3 +9,9 @@ app.get ('/', (req , res) => {
 let hmtlPath = path.resolve (__dirname,'views/home.html');
 res.sendFile(hmtlPath);
 })
+app.get('/login', (req , res)=> {
+res.sendFile(path.resolve(__dirname,'views/login.html'));
+})
+app.get('/register', (req , res)=> {
+    res.sendFile(path.resolve(__dirname,'views/register.html'));
+    })
