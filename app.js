cf915c3efa7c002616ac0d0d147e837c7e0a5b5d
EloisaCB43 <eloisacardona43@gmail.com>
2021-12-13 20:11:44 -0500

    cambios listen

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const res = require('express/lib/response');
 const { dirname } = require('path');
 const app = express();
 const path= require('path')
-app.listen(3000,() => console.log ('servidor corriendo en el puerto 3000'));
+app.listen(process.env.PORT || 3000, function() {
+    console.log ('servidor corriendo en el puerto 3000');
 app.use (express.static(path.resolve(__dirname,'public')));
 app.get ('/', (req , res) => {
 let hmtlPath = path.resolve (__dirname,'views/home.html');
