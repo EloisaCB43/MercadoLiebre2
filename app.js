4822d9160b7ad7963a7529297a8c545b51c3ee27
EloisaCB43 <eloisacardona43@gmail.com>
2021-12-13 20:17:35 -0500

    organice bien mi app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { dirname } = require('path');
 const app = express();
 const path= require('path')
 app.listen(process.env.PORT || 3000, function() {
-    console.log ('servidor corriendo en el puerto 3000');
+    console.log ('servidor corriendo en el puerto 3000');})
 app.use (express.static(path.resolve(__dirname,'public')));
 app.get ('/', (req , res) => {
 let hmtlPath = path.resolve (__dirname,'views/home.html');
@@ -15,4 +15,4 @@ res.sendFile(path.resolve(__dirname,'views/login.html'));
 })
 app.get('/register', (req , res)=> {
     res.sendFile(path.resolve(__dirname,'views/register.html'));
-    })
+})
