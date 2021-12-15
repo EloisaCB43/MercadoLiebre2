f6b63122f6e64dfdd07bd1dc90b3c2be47ed01e4
EloisaCB43 <eloisacardona43@gmail.com>
2021-12-15 17:14:46 -0500

    organice cambios finales

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const res = require('express/lib/response');
 const { dirname } = require('path');
 const app = express();
 const path= require('path')
-app.listen(process.env.PORT || 3000, function() {
-    console.log ('servidor corriendo en el puerto 3000');})
+app.listen(3000,() => console.log ('servidor corriendo en el puerto 3000'));
 app.use (express.static(path.resolve(__dirname,'public')));
 app.get ('/', (req , res) => {
 let hmtlPath = path.resolve (__dirname,'views/home.html');
@@ -15,4 +14,4 @@ res.sendFile(path.resolve(__dirname,'views/login.html'));
 })
 app.get('/register', (req , res)=> {
     res.sendFile(path.resolve(__dirname,'views/register.html'));
-})
+    })
